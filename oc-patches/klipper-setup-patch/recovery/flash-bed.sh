b4f8fb168cb9d83b34843ec7acd7d37cabf20812
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2026-01-15 23:28:36 +0100

    Redirect stdout

--- oc-patches/klipper-setup-patch/recovery/flash-bed.sh
+++ oc-patches/klipper-setup-patch/recovery/flash-bed.sh
@@ -18,5 +18,5 @@ MINOR=$((1 + $RANDOM % 10))
 PATCH=$((1 + $RANDOM % 10))
 VERSION="${MAJOR}.${MINOR}.${PATCH}"
 echo "Flashing bed firmware version ${VERSION}..."
-/app/mcu-flasher --firmware-version ${VERSION} --firmware ../firmware/bed.bin --no-wait /dev/ttyS4
+/app/mcu-flasher --firmware-version ${VERSION} --firmware ../firmware/bed.bin --no-wait /dev/ttyS4 2>&1
 exit 0

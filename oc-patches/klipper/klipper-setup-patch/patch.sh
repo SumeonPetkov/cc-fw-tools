5e500047ca2efb17e98a59fa1e61c3b7f3f55a3a
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2026-03-01 22:30:30 +0100

    Start reworking the patching system

--- oc-patches/klipper/klipper-setup-patch/patch.sh
+++ oc-patches/klipper/klipper-setup-patch/patch.sh
@@ -15,15 +15,6 @@ chmod a+x ./app/klipper/recovery/*
 chmod a+x ./app/klipper/runtime/*
 chmod a+x ./app/klipper/install.sh
 
-# Download binaries
-curl -L -o ./app/mcu-flasher https://github.com/OpenCentauri/OpenCentauri/releases/latest/download/mcu-flasher-linux-armv7
-curl -L -o ./app/serial-multiplexer https://github.com/OpenCentauri/OpenCentauri/releases/latest/download/serial-multiplexer-linux-armv7
-curl -L -o ./app/dsp-to-serial https://github.com/OpenCentauri/OpenCentauri/releases/latest/download/dsp-to-serial-linux-armv7
-curl -L -o ./app/recovery https://github.com/OpenCentauri/OpenCentauri/releases/download/v0.0.0/recovery
-
-# Make sure we can run them
-chmod a+x ./app/mcu-flasher ./app/serial-multiplexer ./app/dsp-to-serial ./app/recovery
-
 # Download firmwares
 cp ../dsp0 ./app/klipper/firmware/official.elf
 curl -L -o ./app/klipper/firmware/bed.bin https://github.com/OpenCentauri/kalico/releases/download/0.0.1/bed.bin
---
+++ oc-patches/services/bootstrap-oc/patch.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+if [ $UID -ne 0 ]; then
+  echo "Error: Please run as root."
+  exit 1
+fi
+
+project_root="$REPOSITORY_ROOT"
+source "$project_root/TOOLS/helpers/utils.sh" "$project_root"
+
+check_tools "grep md5sum openssl wc awk sha256sum mksquashfs git git-lfs"
+
+echo Go into the squashfs-root dir for the rest of the steps!
+cd "$SQUASHFS_ROOT"
+
+set -x
+set -e
+
+echo Check MD5sum on OpenCentauri bootstrap
+BOOTSTRAP_PATH="$CURRENT_PATCH_PATH/OpenCentauri-bootstrap.tar.gz"
+MD5_SUM="$(md5sum "$BOOTSTRAP_PATH" | awk '{print $1}')"
+if [[ ! "$MD5_SUM" = "$OC_BOOTSTRAP_MD5" ]]; then
+  printf "MD5 hash of %s (%s) does not match expected %s, aborting...\n" "$BOOTSTRAP_PATH" "$MD5_SUM" "$OC_BOOTSTRAP_MD5"
+  exit 1
+fi
+
+echo Copy over the OpenCentauri bootstrap tarball to /app
+cp "$CURRENT_PATCH_PATH/OpenCentauri-bootstrap.tar.gz" ./app
+chmod 644 ./app/OpenCentauri-bootstrap.tar.gz
+
+echo 'Add symlink for /lib/modules/ for the 1.1.40 FW w/ new kernel ver 5.4.61-ab1175 (harmless for earlier revs)'
+cd ./lib/modules
+ln -sf 5.4.61 5.4.61-ab1175
+cd -
+
+echo "Install Ethernet kmod(s)"
+cp "$CURRENT_PATCH_PATH/kmod/r8152.ko" "$SQUASHFS_ROOT/lib/modules/5.4.61/"
+cp "$CURRENT_PATCH_PATH/kmod/ax88179_178a.ko" "$SQUASHFS_ROOT/lib/modules/5.4.61/"
+
+echo Installing automatic wifi scripts/automation to run on boot
+# Install oc-startwifi.sh script to /app:
+cat "$CURRENT_PATCH_PATH/oc-startwifi.sh" > ./app/oc-startwifi.sh
+chmod 755 ./app/oc-startwifi.sh
+
+echo Installing automatic NTP date/time sync to run on boot
+cat "$CURRENT_PATCH_PATH/ntpdate" > ./usr/sbin/ntpdate
+chmod 755 ./usr/sbin/ntpdate
+
+# Install 'mount_usb' script in /usr/sbin
+cat "$CURRENT_PATCH_PATH/mount_usb" > ./usr/sbin/mount_usb
+chmod 755 ./usr/sbin/mount_usb
+
+# Install 'mount_usb_daemon' script in /usr/sbin
+cat "$CURRENT_PATCH_PATH/mount_usb_daemon" > ./usr/sbin/mount_usb_daemon
+chmod 755 ./usr/sbin/mount_usb_daemon
+
+cp "$CURRENT_PATCH_PATH/bootstrap-oc" ./etc/init.d/bootstrap-oc
+chmod 755 ./etc/init.d/bootstrap-oc
+ln -s ../init.d/bootstrap-oc ./etc/rc.d/S81bootstrap-oc
+chmod 755 ./etc/rc.d/S81bootstrap-oc

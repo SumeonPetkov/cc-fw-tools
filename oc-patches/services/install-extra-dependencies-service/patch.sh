e494148310287b6e931f8fffe04041c95061726f
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2026-03-07 20:28:42 +0100

    Move extra dependencies service

---
+++ oc-patches/services/install-extra-dependencies-service/patch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+cd "$SQUASHFS_ROOT"
+mkdir ./app/packages
+
+cp "$CURRENT_PATCH_PATH/oc-install-extra-packages" ./etc/init.d/oc-install-extra-packages
+chmod a+x ./etc/init.d/oc-install-extra-packages
+ln -s ../init.d/oc-install-extra-packages ./etc/rc.d/S82oc-install-extra-packages
+chmod a+x ./etc/rc.d/S82oc-install-extra-packages
+
+cd "$CURRENT_PATCH_PATH"
+cp ./packages/* "$SQUASHFS_ROOT/app/packages"
+
+cat ./rc.local >> "$SQUASHFS_ROOT/etc/rc.local"
